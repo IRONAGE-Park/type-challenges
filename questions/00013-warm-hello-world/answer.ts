7547d6cda2e15e9c92aeecaa7523d67e76543c6a
CheolHyun Park <43946503+IRONAGE-Park@users.noreply.github.com>
2023-04-15 15:07:13 +0900

    Update answer.ts

--- questions/00013-warm-hello-world/answer.ts
+++ questions/00013-warm-hello-world/answer.ts
@@ -1,16 +1,9 @@
-type HelloWorld = string // expected to be a string
+type HelloWorld = string;
 
 /* _____________ 테스트 케이스 _____________ */
-import type { Equal, Expect, NotAny } from '@type-challenges/utils'
+import type { Equal, Expect, NotAny } from '@type-challenges/utils';
 
 type cases = [
   Expect<NotAny<HelloWorld>>,
   Expect<Equal<HelloWorld, string>>,
-]
-
-/* _____________ 다음 단계 _____________ */
-/*
-  > 정답 공유하기: https://tsch.js.org/13/answer/ko
-  > 정답 보기: https://tsch.js.org/13/solutions
-  > 다른 문제들: https://tsch.js.org/ko
-*/
+];
