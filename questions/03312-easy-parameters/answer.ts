712da917cbf932ddf71456eb0701f9c4a6b8ed2c
CheolHyun Park <43946503+IRONAGE-Park@users.noreply.github.com>
2023-04-23 00:40:06 +0900

    Create answer.ts

---
+++ questions/03312-easy-parameters/answer.ts
@@ -0,0 +1,14 @@
+type MyParameters<T> = T extends (...args: infer R) => unknown ? R : never;
+
+/* _____________ 테스트 케이스 _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+const foo = (arg1: string, arg2: number): void => {};
+const bar = (arg1: boolean, arg2: { a: 'A' }): void => {};
+const baz = (): void => {};
+
+type cases = [
+  Expect<Equal<MyParameters<typeof foo>, [string, number]>>,
+  Expect<Equal<MyParameters<typeof bar>, [boolean, { a: 'A' }]>>,
+  Expect<Equal<MyParameters<typeof baz>, []>>,
+];
