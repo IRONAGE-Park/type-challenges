c235c48594703dd9fbee077e784327c957c3d123
CheolHyun Park <43946503+IRONAGE-Park@users.noreply.github.com>
2023-04-22 18:00:16 +0900

    00014-easy-first

---
+++ questions/00014-easy-first/answer.ts
@@ -0,0 +1,18 @@
+type First<T extends unknown[]> = T extends [infer P, ...unknown[]] ? P : never;
+
+/* _____________ 테스트 케이스 _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<First<[3, 2, 1]>, 3>>,
+  Expect<Equal<First<[() => 123, { a: string }]>, () => 123>>,
+  Expect<Equal<First<[]>, never>>,
+  Expect<Equal<First<[undefined]>, undefined>>,
+]
+
+type errors = [
+  // @ts-expect-error
+  First<'notArray'>,
+  // @ts-expect-error
+  First<{ 0: 'arrayLike' }>,
+]
