591db0b6ecfe2425138e87dd8f740d0fa74b5b79
CheolHyun Park <43946503+IRONAGE-Park@users.noreply.github.com>
2023-04-23 00:41:00 +0900

    Create answer.ts (#8)

---
+++ questions/00043-easy-exclude/answer.ts
@@ -0,0 +1,10 @@
+type MyExclude<T, U> = T extends U ? never : T;
+
+/* _____________ 테스트 케이스 _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<MyExclude<'a' | 'b' | 'c', 'a'>, 'b' | 'c'>>,
+  Expect<Equal<MyExclude<'a' | 'b' | 'c', 'a' | 'b'>, 'c'>>,
+  Expect<Equal<MyExclude<string | number | (() => void), Function>, string | number>>,
+];
