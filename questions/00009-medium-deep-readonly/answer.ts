1b4beb1c3ba21ab75b50f15d5da8e99785e81786
CheolHyun Park <43946503+IRONAGE-Park@users.noreply.github.com>
2023-05-02 10:39:12 +0900

    Create answer.ts

---
+++ questions/00009-medium-deep-readonly/answer.ts
@@ -0,0 +1,61 @@
+type DeepReadonly<T extends {}> = T extends Function ? T : {
+  readonly [k in keyof T]: T[k] extends {} ? DeepReadonly<T[k]> : T[k];
+};
+
+/* _____________ 테스트 케이스 _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<DeepReadonly<X1>, Expected1>>,
+  Expect<Equal<DeepReadonly<X2>, Expected2>>,
+];
+
+type X1 = {
+  a: () => 22
+  b: string
+  c: {
+    d: boolean
+    e: {
+      g: {
+        h: {
+          i: true
+          j: 'string'
+        }
+        k: 'hello'
+      }
+      l: [
+        'hi',
+        {
+          m: ['hey']
+        },
+      ]
+    }
+  }
+};
+
+type X2 = { a: string } | { b: number };
+
+type Expected1 = {
+  readonly a: () => 22
+  readonly b: string
+  readonly c: {
+    readonly d: boolean
+    readonly e: {
+      readonly g: {
+        readonly h: {
+          readonly i: true
+          readonly j: 'string'
+        }
+        readonly k: 'hello'
+      }
+      readonly l: readonly [
+        'hi',
+        {
+          readonly m: readonly ['hey']
+        },
+      ]
+    }
+  }
+};
+
+type Expected2 = { readonly a: string } | { readonly b: number };
