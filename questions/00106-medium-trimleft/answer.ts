d2492813953ee8baec21a12655e46c57d1f441ee
CheolHyun Park <43946503+IRONAGE-Park@users.noreply.github.com>
2023-05-11 15:28:58 +0900

    Create answer.ts

---
+++ questions/00106-medium-trimleft/answer.ts
@@ -0,0 +1,16 @@
+type Blank = ' ' | '\n' | '\t';
+
+type TrimLeft<S extends string> = S extends `${infer C}${infer T}` ? C extends Blank ? TrimLeft<T> : S : S;
+
+/* _____________ 테스트 케이스 _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<TrimLeft<'str'>, 'str'>>,
+  Expect<Equal<TrimLeft<' str'>, 'str'>>,
+  Expect<Equal<TrimLeft<'     str'>, 'str'>>,
+  Expect<Equal<TrimLeft<'     str     '>, 'str     '>>,
+  Expect<Equal<TrimLeft<'   \n\t foo bar '>, 'foo bar '>>,
+  Expect<Equal<TrimLeft<''>, ''>>,
+  Expect<Equal<TrimLeft<' \n\t'>, ''>>,
+];
