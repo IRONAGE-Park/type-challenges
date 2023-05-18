41e4e451cc0a45212c1645041287f77064ff428a
CheolHyun Park <43946503+IRONAGE-Park@users.noreply.github.com>
2023-05-19 07:44:56 +0900

    Create answer.ts

---
+++ questions/00298-medium-length-of-string/answer.ts
@@ -0,0 +1,14 @@
+type LengthOfString<S extends string, L extends unknown[] = []> =
+  S extends `${string}${infer R}`
+    ? LengthOfString<R, [...L, unknown]>
+    : L['length'];
+
+/* _____________ 테스트 케이스 _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<LengthOfString<''>, 0>>,
+  Expect<Equal<LengthOfString<'kumiko'>, 6>>,
+  Expect<Equal<LengthOfString<'reina'>, 5>>,
+  Expect<Equal<LengthOfString<'Sound! Euphonium'>, 16>>,
+];
