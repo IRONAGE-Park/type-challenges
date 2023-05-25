701a78c1a5d0bbe4e85d9342601c4af7ee943361
CheolHyun Park <43946503+IRONAGE-Park@users.noreply.github.com>
2023-05-25 11:10:14 +0900

    Create answer.ts

---
+++ questions/00531-medium-string-to-union/answer.ts
@@ -0,0 +1,16 @@
+type StringToUnion<T extends string> =
+  T extends ""
+    ? never
+    : T extends `${infer U}${infer Rest}`
+      ? U | StringToUnion<Rest>
+      : T;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<StringToUnion<''>, never>>,
+  Expect<Equal<StringToUnion<'t'>, 't'>>,
+  Expect<Equal<StringToUnion<'hello'>, 'h' | 'e' | 'l' | 'l' | 'o'>>,
+  Expect<Equal<StringToUnion<'coronavirus'>, 'c' | 'o' | 'r' | 'o' | 'n' | 'a' | 'v' | 'i' | 'r' | 'u' | 's'>>,
+];
