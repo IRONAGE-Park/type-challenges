7a4546a838042eb59a7147043c7b498580394b56
CheolHyun Park <43946503+IRONAGE-Park@users.noreply.github.com>
2023-05-28 13:51:14 +0900

    Create answer.ts

---
+++ questions/00612-medium-kebabcase/answer.ts
@@ -0,0 +1,26 @@
+type Uppers = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H' | 'I' | 'J' | 'K' | 'L' | 'M' | 'N' | 'O' | 'P' | 'Q' | 'R' | 'S' | 'T' | 'U' | 'V' | 'W' | 'X' | 'Y' | 'Z';
+
+type Transformer<S extends string> = S extends `${infer F}${infer Rest}`
+  ? F extends Uppers
+    ? `-${Lowercase<F>}${Transformer<Rest>}`
+    : `${F}${Transformer<Rest>}`
+  : S;
+
+type KebabCase<S> = S extends `${infer F}${infer Rest}`
+  ? Transformer<`${Lowercase<F>}${Rest}`>
+  : '';
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type cases = [
+  Expect<Equal<KebabCase<'FooBarBaz'>, 'foo-bar-baz'>>,
+  Expect<Equal<KebabCase<'fooBarBaz'>, 'foo-bar-baz'>>,
+  Expect<Equal<KebabCase<'foo-bar'>, 'foo-bar'>>,
+  Expect<Equal<KebabCase<'foo_bar'>, 'foo_bar'>>,
+  Expect<Equal<KebabCase<'Foo-Bar'>, 'foo--bar'>>,
+  Expect<Equal<KebabCase<'ABC'>, 'a-b-c'>>,
+  Expect<Equal<KebabCase<'-'>, '-'>>,
+  Expect<Equal<KebabCase<''>, ''>>,
+  Expect<Equal<KebabCase<'😎'>, '😎'>>,
+];
